9bb28e915e2b3d05cf724b7dcbe1d2ec3fa9e84f
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-24 13:21:36 -0800

    Update Home.js

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,17 +1,21 @@
+// Home.js: Component for rendering the home page
 import React from "react";
 import { useHistory } from "react-router-dom";
 
 function Home() {
   let history = useHistory();
 
+  // Navigate to the create deck page when the "+ Create" button is clicked
   function handleCreateDeck(event) {
     event.preventDefault();
     history.push("/decks/new");
   }
 
+  // Render the Home component
   return (
     <div>
       <div>
+        {/* Button to navigate to the create deck page */}
         <button
           type="button"
           className="btn btn-secondary m-2"
@@ -25,3 +29,4 @@ function Home() {
 }
 
 export default Home;
+

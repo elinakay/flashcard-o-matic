083ebb826e4259fef17447455512585935425c11
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-24 13:18:57 -0800

    Update DeckList.js

--- src/Layout/Decks/DeckList.js
+++ src/Layout/Decks/DeckList.js
@@ -1,3 +1,4 @@
+// DeckList.js: Component for rendering a list of decks
 import React, { useEffect, useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { listDecks } from "../../utils/api";
@@ -8,12 +9,14 @@ export const DeckList = () => {
   const [error, setError] = useState(undefined);
   const [decks, setDecks] = useState([]);
 
+  // Load the list of decks when the component mounts
   useEffect(() => {
     const abortController = new AbortController();
     listDecks(abortController.signal).then(setDecks).catch(setError);
     return () => abortController.abort();
   }, []);
 
+  // Display an error message if loading decks fails
   if (error) {
     return (
       <ErrorMessage error={error}>
@@ -24,9 +27,12 @@ export const DeckList = () => {
     );
   }
 
+  // Map each deck to a Deck component
   const list = decks.map((deck) => <Deck key={deck.id} deck={deck} />);
 
+  // Render the DeckList component
   return <>{list}</>;
 };
 
 export default DeckList;
+

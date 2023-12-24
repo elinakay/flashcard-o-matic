8056e3acc51dd9dfc1fb59bd006f94fd09184770
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-24 13:13:32 -0800

    Update EditCard.js

--- src/Layout/Cards/EditCard.js
+++ src/Layout/Cards/EditCard.js
@@ -1,3 +1,4 @@
+// EditCard.js: Component for editing an existing flashcard
 import React, { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import CardForm from "./CardForm";
@@ -7,6 +8,7 @@ function EditCard({ deck, setDeck, card, setCard }) {
   const { deckId, cardId } = useParams();
   const history = useHistory();
 
+  // Load the deck data when the component mounts
   useEffect(() => {
     async function loadDeck() {
       const loadedDeck = await readDeck(deckId);
@@ -15,6 +17,7 @@ function EditCard({ deck, setDeck, card, setCard }) {
     loadDeck();
   }, [deckId, setDeck]);
 
+  // Load the card data when the component mounts
   useEffect(() => {
     async function loadCard() {
       const cardRead = await readCard(cardId);
@@ -23,25 +26,31 @@ function EditCard({ deck, setDeck, card, setCard }) {
     loadCard();
   }, [cardId, setCard]);
 
+  // Update the front value of the card on input change
   function changeFront(event) {
     setCard({ ...card, front: event.target.value });
   }
 
+  // Update the back value of the card on input change
   function changeBack(event) {
     setCard({ ...card, back: event.target.value });
   }
 
+  // Save the updated card and redirect to the deck view
   function handleSave(event) {
     event.preventDefault();
     updateCard(card).then((response) => history.push(`/decks/${deck.id}`));
   }
 
+  // Redirect to the deck view without saving changes
   function handleDone() {
     history.push(`/decks/${deck.id}`);
   }
 
+  // Render the EditCard component
   return (
     <div>
+      {/* Breadcrumb navigation */}
       <div>
         <nav aria-label="breadcrumb">
           <ol className="breadcrumb">
@@ -73,3 +82,4 @@ function EditCard({ deck, setDeck, card, setCard }) {
 }
 
 export default EditCard;
+

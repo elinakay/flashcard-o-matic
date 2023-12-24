89bca632150982bd56d62f704190bb9cf696c702
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-24 13:24:06 -0800

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,3 +1,4 @@
+// Layout.js: Main layout component containing the application routes
 import React, { Fragment, useState } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
@@ -12,31 +13,42 @@ import EditCard from "./Cards/EditCard";
 import Home from "./Home";
 
 function Layout() {
+  // State variables for managing decks, a specific deck, and a card
   const [decks, setDecks] = useState([]);
   const [deck, setDeck] = useState({});
   const [card, setCard] = useState([]);
 
+  // Render the layout of the application
   return (
     <>
+      {/* Display the application header */}
       <Header />
+      {/* Main container for rendering different components based on the route */}
       <div className="container">
+        {/* Switch component for handling different routes */}
         <Switch>
+          {/* Home route: Display the home page and deck list */}
           <Route exact path="/">
             <Home decks={decks} setDecks={setDecks} />
             <DeckList decks={decks} setDecks={setDecks} />
           </Route>
+          {/* Create Deck route: Display the create deck form */}
           <Route path="/decks/new">
             <CreateDeck />
           </Route>
+          {/* View Deck route: Display the details of a specific deck */}
           <Route exact path="/decks/:deckId">
             <DeckView />
           </Route>
+          {/* Study Deck route: Display the study page for a specific deck */}
           <Route path="/decks/:deckId/study">
             <DeckStudy />
           </Route>
+          {/* Edit Deck route: Display the edit form for a specific deck */}
           <Route path="/decks/:deckId/edit">
             <DeckEdit deck={deck} setDeck={setDeck} />
           </Route>
+          {/* Add Card route: Display the form for adding a card to a deck */}
           <Route path="/decks/:deckId/cards/new">
             <AddCard
               card={card}
@@ -45,6 +57,7 @@ function Layout() {
               setDeck={setDeck}
             />
           </Route>
+          {/* Edit Card route: Display the form for editing a specific card */}
           <Route exact path="/decks/:deckId/cards/:cardId/edit">
             <EditCard
               deck={deck}
@@ -53,6 +66,7 @@ function Layout() {
               setCard={setCard}
             />
           </Route>
+          {/* Default route: Display the Not Found page for unrecognized routes */}
           <Route>
             <NotFound />
           </Route>
@@ -63,3 +77,4 @@ function Layout() {
 }
 
 export default Layout;
+

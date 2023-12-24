766e4410ed0f7b1a984922954591d9aa1d2c07e0
Elina Kayumova <129918798+elinakay@users.noreply.github.com>
2023-12-24 13:20:50 -0800

    Update ErrorMessage.js

--- src/Layout/ErrorMessage.js
+++ src/Layout/ErrorMessage.js
@@ -1,10 +1,13 @@
+// ErrorMessage.js: Component for displaying error messages
 import React from "react";
 
 export const ErrorMessage = ({ error, children }) => (
   <main className="container">
+    {/* Display the error message in red */}
     <p style={{ color: "red" }}>ERROR: {error.message}</p>
     {children}
   </main>
 );
 
 export default ErrorMessage;
+
